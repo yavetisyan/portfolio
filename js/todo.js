24a2ea4fcf4c358180c3d679bf2f1b80c3c286f3
yavetisyan <mr.avetisyan@yahoo.com>
2021-09-18 23:21:08 +0400

    add data

--- js/todo.js
+++ js/todo.js
@@ -13,31 +13,55 @@ input.addEventListener("keyup", function (e) {
 
 btn.addEventListener("click", function () {
   let add = document.querySelector(".addInput");
-  let newDiv = document.createElement("li");
+  let newDiv = document.createElement("div");
+  let newLi = document.createElement("li");
   let newP = document.createElement("p");
   let removBtn = document.createElement("span");
+  let dataDiv = document.createElement("div");
+  let dataList = document.querySelector(".data__list");
+  let timeList = document.querySelector(".time__list");
+  let span = document.createElement("span");
 
   if (input.value !== "") {
-    newDiv.className = "list__title";
+    newDiv.className = "div__title";
+    newLi.className = "list__title";
     newP.className = "list";
     removBtn.className = "icon-cancel-circle";
     add.prepend(newDiv);
-    newDiv.prepend(removBtn);
-    newDiv.prepend(newP);
+    newDiv.prepend(newLi);
+    newDiv.append(dataDiv);
+
+    newLi.prepend(removBtn);
+    newLi.prepend(newP);
     newP.innerText = input.value;
+    dataDiv.classList.add("data");
+    dataDiv.innerHTML = getDate();
+
+    //dataDiv.appendChild(document.createElement("span").classList.add(dataList));
+
     input.value = "";
   }
 
   //remove
 
   let close = document.getElementsByClassName("icon-cancel-circle");
+  console.log(close);
   for (let i = 0; i < close.length; i++) {
-    console.log(close[i]);
     close[i].addEventListener("click", function () {
-      this.parentElement.remove();
+      this.parentElement.parentElement.remove();
     });
+  }
 
-    // search
+  // date
+
+  function getDate() {
+    let dateObj = new Date();
+    let month = dateObj.getUTCMonth() + 1; //months from 1-12.
+    let day = dateObj.getUTCDate();
+    let year = dateObj.getUTCFullYear();
+    let newdate = year + "/" + month + "/" + day;
+
+    return `${newdate}  -  ${dateObj.getHours()} : ${dateObj.getMinutes()} : ${dateObj.getSeconds()}`;
   }
 
   // toogle list
@@ -46,5 +70,3 @@ btn.addEventListener("click", function () {
     newP.classList.toggle("list-active");
   });
 });
-
-// search value
